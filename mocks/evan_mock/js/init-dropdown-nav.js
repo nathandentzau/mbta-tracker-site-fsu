41c9a6330a09e5780e03c49b38259555cd595e64
Sherwyn <wedydit@gmail.com>
2016-03-09 09:51:48 -0500

    Added a java script file that is used for the drop down menu action and linked it to evans green.html file. Also made a couple changes in the green.html file so that his mobile nav worked. I left some notes :-). Evan your going to need to link to that init-dropdown-nav.js file on every page you want the drop down and also just copy and pace the new header to those files as well... eventually we probbaly will use php or javascript to include the nav on every new page.

---
+++ mocks/evan_mock/js/init-dropdown-nav.js
@@ -0,0 +1,9 @@
+// This is to get drop down to work :) -Sherwyn
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
